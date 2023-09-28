1cf35767800c24694d01179b54f44c6233d01b21
sudhanva rajesh <sudhanvarajesh@tamu.edu>
2023-09-28 18:36:33 -0500

    UI changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
         session[:user_id] = @user.id
         redirect_to root_url, notice: "Logged in successfully!"
       else
-        flash.now[:error] = "Invalid email or password"
+        flash[:error] = "Invalid email or password"
         redirect_to sessions_new_path
       end
     end
