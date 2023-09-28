326311d551aee20233ea7fada7ecda62aa78deb4
Eric <108982075+zepuma@users.noreply.github.com>
2023-09-27 23:02:37 -0500

    mailer feature added

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get 'passward/reset/edit', to 'password_resets#edit'
+  # patch 'passward/reset/edit', to 'password_resets#update'
+
   root 'welcome#index'
 end
