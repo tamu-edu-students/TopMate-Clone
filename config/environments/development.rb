778723519dcf39de4d4f1fbf62a2a9b4dd25f653
Eric <108982075+zepuma@users.noreply.github.com>
2023-09-29 13:58:11 -0500

    default sending email address set in config/environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   
+  # Change the email address to the one you want to use for testing
+  ActionMailer::Base.default :from => 'default@development-server.com'
+  
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
