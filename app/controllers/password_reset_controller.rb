db1948624c746f391cab7516f652219a42352930
Geetesh <59590206+geeteshCh@users.noreply.github.com>
2023-10-02 11:03:52 -0500

    minor changes

--- app/controllers/password_reset_controller.rb
+++ app/controllers/password_reset_controller.rb
@@ -1,26 +1,26 @@
 class PasswordResetController < ApplicationController
     def change_password
-        puts "resetting password for token #{params[:token]}"
+        # puts "resetting password for token #{params[:token]}"
 
         sessionToken = ResetPasswordSession.find_by(session_token: params[:token])
         if sessionToken
           # Reset password logic
-          puts "retrieved session token id is #{sessionToken.id}"
-          puts sessionToken.inspect
+          # puts "retrieved session token id is #{sessionToken.id}"
+          # puts sessionToken.inspect
         
           user = User.find(sessionToken.user_id)
-          puts user.inspect
-          puts "retrieved user name is #{user.fname}"
+          # puts user.inspect
+          # puts "retrieved user name is #{user.fname}"
           user.password = params[:password]
           
           user.save
 
           flash[:success] = 'Password changed'
           sessionToken.destroy
-          puts "session deleted"
+          # puts "session deleted"
 
         else
-          puts "session not found for token #{params[:token]}"
+          # puts "session not found for token #{params[:token]}"
 
           flash[:error] = "Reset session does not exist"
         end
