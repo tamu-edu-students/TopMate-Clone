b3c66c5231f648b16a077307420ffaeec0f30454
sudhanva rajesh <sudhanvarajesh@tamu.edu>
2023-10-12 15:31:22 -0500

    added rspec and cucumber tests, and modified services controller

--- Gemfile
+++ Gemfile
@@ -3,8 +3,8 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.2.2'
-
+ruby "3.2.2"
+gem 'rails-controller-testing'
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.7', '>= 7.0.7.2'
 
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,26 +1,35 @@
 class ServicesController < ApplicationController
     def new
       @current_user ||= User.find_by(user_id: session[:user_id])
-      redirect_to login_url if @current_user.nil?
-      @service = Service.new
+      if @current_user.nil?
+        redirect_to login_url 
+      else
+        @service = Service.new
+      end
     end
   
     def create
       @current_user ||= User.find_by(user_id: session[:user_id])
-      redirect_to login_url if @current_user.nil?
-      @service = @current_user.services.new(service_params)
-      @service.user_id = @current_user.user_id
-      if @service.save
-        redirect_to root_path, notice: "Service created successfully."
+      if @current_user.nil?
+        redirect_to login_url 
       else
-        render :new
+        @service = @current_user.services.new(service_params)
+        @service.user_id = @current_user.user_id
+        if @service.save
+          redirect_to root_path, notice: "Service created successfully."
+        else
+          render :new
+        end
       end
     end
 
     def index
       @current_user ||= User.find_by(user_id: session[:user_id])
-      redirect_to login_url if @current_user.nil?
-      @services = @current_user.services
+      if @current_user.nil?
+        redirect_to login_url 
+      else
+        @services = @current_user.services
+      end
     end
 
     private
