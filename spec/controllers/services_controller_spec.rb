e3fc2e7affcf36520689ea5c935e8079a42b9b2e
sudhanva rajesh <sudhanvarajesh@tamu.edu>
2023-10-12 15:31:22 -0500

    added rspec and cucumber tests, and modified services controller

--- spec/controllers/services_controller_spec.rb
+++ spec/controllers/services_controller_spec.rb
@@ -1,29 +1,89 @@
-require 'rails_helper'
-
-RSpec.feature 'Services', type: :feature do
-  let(:user) { create(:user) }
-
-  scenario 'User creates a new service' do
-    # Log in the user (assuming you have authentication set up)
-    login_as(user, scope: :user)
-
-    # Visit the index page
-    visit services_path
-
-    # Click on the "New Service" button
-    click_link 'New Service'
-
-    # Fill in the form with service details
-    fill_in 'Name', with: 'New Service'
-    # Fill in other fields as needed
-
-    # Submit the form
-    click_button 'Create Service'
-
-    # Expectations
-    expect(page).to have_text('Service created successfully.')
-    expect(page).to have_text('New Service')
-
-    # Additional expectations as needed
+RSpec.describe ServicesController, type: :controller do
+  describe "GET #new" do
+    context "when user is logged in" do
+      let(:user) { User.create(fname: "John", lname: "Doe", email: "test@example.com", password: "password") }
+      
+      before do
+        session[:user_id] = user.user_id
+        get :new
+      end
+      
+      it "assigns a new service" do
+        expect(assigns(:service)).to be_a_new(Service)
+      end
+    end
+    
+    context "when user is not logged in" do
+      before { get :new }
+      
+      it "redirects to login_url" do
+        expect(response).to redirect_to(login_path)
+      end
+    end
+  end
+  
+  describe "POST #create" do
+    let(:user) { User.create(fname: "John", lname: "Doe", email: "test@example.com", password: "password") }
+    
+    context "when user is logged in" do
+      before { session[:user_id] = user.user_id }
+      
+      context "with valid parameters" do
+        let(:valid_params) { { service: { name: "Test Service", description: "test service description", price: 120, duration:10 } } }
+        
+        it "creates a new service" do
+          expect {
+            post :create, params: valid_params
+          }.to change(Service, :count).by(1)
+        end
+        
+        it "redirects to root_path" do
+          post :create, params: valid_params
+          expect(response).to redirect_to(root_path)
+        end
+        
+        it "sets a success notice" do
+          post :create, params: valid_params
+          expect(flash[:notice]).to eq("Service created successfully.")
+        end
+      end
+    end
+    
+    context "when user is not logged in" do
+      before { post :create }
+      
+      it "redirects to login_url" do
+        expect(response).to redirect_to(login_path)
+      end
+    end
+  end
+  
+  describe "GET #index" do
+    context "when user is logged in" do
+      let(:user) { User.create(fname: "John", lname: "Doe", email: "test@example.com", password: "password") }
+      let!(:service1) { user.services.create(name: "Test Service1", description: "test service1 description", price: 120, duration:10) }
+      let!(:service2) { user.services.create(name: "Test Service2", description: "test service2 description", price: 20, duration:20) }
+      
+      before do
+        session[:user_id] = user.user_id
+        get :index
+      end
+      
+      it "assigns the user's services" do
+        expect(assigns(:services)).to eq([service1, service2])
+      end
+      
+      it "renders the index template" do
+        expect(response).to render_template(:index)
+      end
+    end
+    
+    context "when user is not logged in" do
+      before { get :index }
+      
+      it "redirects to login_url" do
+        expect(response).to redirect_to(login_path)
+      end
+    end
   end
 end
