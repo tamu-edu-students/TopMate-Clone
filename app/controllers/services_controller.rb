483d6a1f80e96f5b7f375da10f5f1b80947cca39
Geetesh <59590206+geeteshCh@users.noreply.github.com>
2023-10-12 22:23:24 -0700

    updated edit table functionality

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -34,49 +34,63 @@ class ServicesController < ApplicationController
 
 
     def edit_intial
-      @service = Service.find_by(id: params[:token])
+      @current_user ||= User.find_by(user_id: session[:user_id])
+      if @current_user.nil?
+        redirect_to login_url 
+      else
+        @service = Service.find_by(id: params[:token])
       
-      if @service.nil?
-        render plain: "Service with ID #{params[:token]} does not exist."
-      else 
-        render :edit_service
+        if @service.nil?
+          render plain: "Service with ID #{params[:token]} does not exist."
+        else 
+          render :edit_service
+        end
       end
+      
     end
       
       
     def edit
 
+
       puts "inside edit post route"
 
-      @service = Service.find_by(id: params[:token])
-
-      puts "searched for the service"
-
-      if @service.nil?
-        puts "service not available" 
-        flash[:error] = "Service not found"
-        render plain: "Service with ID #{params[:token]} does not exist."
-
-        # redirect_back(fallback_location: root_path)
+      @current_user ||= User.find_by(user_id: session[:user_id])
+      if @current_user.nil?
+        redirect_to login_url 
       else
-        # Update service attributes one by one
-        puts "updating the services"
-        
-        # @service.name = params[:name] if params[:name].present?
-        # @service.description = params[:description] if params[:description].present?
-        # @service.price = params[:price] if params[:price].present?
-        # @service.duration = params[:duration] if params[:duration].present?
+            @service = Service.find_by(id: params[:token])
 
-
-    
-        if @service.update(service_params)
-          puts "updated the service succesfully"
-          redirect_to servicesindex_url
-        else
-          flash[:error] = "Failed to update service"
-          redirect_back(fallback_location: root_path)
-        end
+            puts "searched for the service"
+      
+            if @service.nil?
+              puts "service not available" 
+              flash[:error] = "Service not found"
+              render plain: "Service with ID #{params[:token]} does not exist."
+      
+              # redirect_back(fallback_location: root_path)
+            else
+              # Update service attributes one by one
+              puts "updating the services"
+              
+              # @service.name = params[:name] if params[:name].present?
+              # @service.description = params[:description] if params[:description].present?
+              # @service.price = params[:price] if params[:price].present?
+              # @service.duration = params[:duration] if params[:duration].present?
+      
+      
+          
+              if @service.update(service_params)
+                puts "updated the service succesfully"
+                redirect_to servicesindex_url
+              else
+                flash[:error] = "Failed to update service"
+                redirect_back(fallback_location: root_path)
+              end
+            end
       end
+
+
       puts "completed redirecting"
     end
 
