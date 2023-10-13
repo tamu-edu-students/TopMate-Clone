7b9511b8b9111b03b77613e8ebbec196bf5de825
Braeden Smith <braedensmith629@gmail.com>
2023-10-12 22:13:10 -0500

    Update factory

--- spec/factories/hours.rb
+++ spec/factories/hours.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :hour do
     user_id { "" }
     day { 1 }
-    start_time { "2023-10-11 22:22:18" }
-    end_time { "2023-10-11 22:22:18" }
+    start_time { "2000-01-01 12:00:00" }
+    end_time { "2000-01-01 13:00:00" }
   end
 end
