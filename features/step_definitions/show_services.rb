a2fbb7b88118419531c6a35f0b42bac0cd2f4ee0
Braeden Smith <braedensmith629@gmail.com>
2023-10-27 14:17:23 -0500

    Update cucumber to not sign in user

--- features/step_definitions/show_services.rb
+++ features/step_definitions/show_services.rb
@@ -1,17 +1,11 @@
 Given("I am on the user's public page") do
-  @user = User.create(fname: 'John', lname: 'Doe', email: 'test@example.com', password: 'password')
-  @user.save!
-
-  visit login_path
-  fill_in 'Email', with: 'test@example.com'
-  fill_in 'Password', with: 'password'
-  click_button 'Login'
-  
+  user = User.create(fname: 'John', lname: 'Doe', email: 'test@example.com', password: 'password')
+  user.save!
   service = Service.create(name: 'Service 1', description: 'sessions of 39 hr', price: 770, duration: 770,
-    user_id: @user.id, is_published: true)
+    user_id: user.id, is_published: true)
   service.save!
 
-  visit public_page_path(@user.fname)
+  visit public_page_path(user.fname)
 end
 
 When('I click on {string} link') do |string|
