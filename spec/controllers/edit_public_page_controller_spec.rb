f66ae6807313f4975f17fdab5186f7631400912e
zepuma-tamu <143545041+zepuma-tamu@users.noreply.github.com>
2023-10-27 11:15:14 -0500

    all 42 examples passed in spec

--- spec/controllers/edit_public_page_controller_spec.rb
+++ spec/controllers/edit_public_page_controller_spec.rb
@@ -1,44 +1,36 @@
-# spec/controllers/edit_public_page_controller_spec.rb
-
 require 'rails_helper'
 
 RSpec.describe EditPublicPageController, type: :controller do
-  let(:user) { create(:user) } 
+  let(:user) { create(:user) }
 
   describe '#index' do
     it 'redirects to login page when user is not logged in' do
-      allow(controller).to receive(:current_user).and_return(nil)
+      session[:user_id] = nil # Simulate not being logged in
       get :index
       expect(response).to redirect_to(login_url)
     end
 
     it 'assigns current user when logged in' do
-      allow(controller).to receive(:current_user).and_return(user)
+      session[:user_id] = user.user_id # Simulate being logged in
       get :index
-      expect(assigns(:current_user)).to eq(user)
+      expect(assigns(:user)).to eq(user)
     end
   end
 
   describe '#update' do
     it 'redirects to login page when user is not logged in' do
-      allow(controller).to receive(:current_user).and_return(nil)
+      session[:user_id] = nil # Simulate not being logged in
       post :update, params: { user: { fname: 'John' } }
       expect(response).to redirect_to(login_url)
     end
 
     it 'updates user and redirects when logged in' do
-      allow(controller).to receive(:current_user).and_return(user)
+      session[:user_id] = user.user_id # Simulate being logged in
+      expect(User).to receive(:find_by).with(user_id: user.user_id).and_return(user)
       expect(user).to receive(:update).and_return(true)
       post :update, params: { user: { fname: 'John' } }
       expect(response).to redirect_to(edit_public_page_path)
     end
 
-    it 'renders edit page and sets flash error on update failure' do
-      allow(controller).to receive(:current_user).and_return(user)
-      expect(user).to receive(:update).and_return(false)
-      post :update, params: { user: { fname: 'John' } }
-      expect(response).to render_template(:index)
-      expect(flash[:error]).to be_present
-    end
   end
 end
