e0ba44d1ce813904b100b3314165c391353f71e9
Braeden Smith <braedensmith629@gmail.com>
2023-11-08 15:18:33 -0600

    Validate price precision

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,4 +3,12 @@
 class Service < ApplicationRecord
   belongs_to :user
   has_many :appointment
+
+  validate :price_is_valid_precision
+
+  def price_is_valid_precision
+    if price.to_f != price.to_f.round(2)
+      errors.add(:price, "is invalid. Too many decimal places.")
+    end
+  end
 end
