28e0b40777f4de0e88a0ee83667189c6d6b30b03
Braeden Smith <braedensmith629@gmail.com>
2023-11-07 19:27:07 -0600

    Added 15-minute increment validation

--- app/models/hour.rb
+++ app/models/hour.rb
@@ -7,4 +7,14 @@ class Hour < ApplicationRecord
   validates :day, comparison: { greater_than_or_equal_to: 0, less_than: 7 }
   validates :start_time, presence: true
   validates :end_time, comparison: { greater_than: :start_time }
+  validate :times_must_be_fifteen_min_intervals
+
+  def times_must_be_fifteen_min_intervals
+    if start_time.min % 15 != 0
+      errors.add(:start_time, "start time must be on 15-minute interval")
+    end
+    if end_time.min % 15 != 0
+      errors.add(:end_time, "start time must be on 15-minute interval")
+    end
+  end
 end
