c733b366507e64df1e4ab176fbd976932f0b6717
Braeden Smith <braedensmith629@gmail.com>
2023-11-08 22:36:38 -0600

    Updated home controller to better enforce redirect

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  before_action :is_logged_in
-  helper_method :current_user
+  before_action :get_current_user
+  before_action :redirect_if_logged_in
 
-  def current_user
+  def index; end
+  
+  private
+
+  def get_current_user
     @current_user ||= User.find_by(user_id: session[:user_id])
   end
-
-  def require_login
-    redirect_to login_url, alert: 'You must be logged in to access this page.' if current_user.nil?
-  end
-
-  def is_logged_in
-    redirect_to dashboard_url if current_user
+  
+  def redirect_if_logged_in
+    redirect_to dashboard_url if !@current_user.nil?
   end
 end
