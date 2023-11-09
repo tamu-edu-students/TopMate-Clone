8ff65ec2da1434101c283361deb0ebb8764cac34
Braeden Smith <braedensmith629@gmail.com>
2023-11-08 20:11:19 -0600

    Updated security for edit public page

--- app/controllers/edit_public_page_controller.rb
+++ app/controllers/edit_public_page_controller.rb
@@ -1,22 +1,13 @@
 # frozen_string_literal: true
 
 class EditPublicPageController < ApplicationController
-  def index
-    return unless !flash[:error].present? || !flash[:error].present?
-
-    @current_user ||= User.find_by(user_id: session[:user_id])
-    if @current_user.nil?
-      redirect_to login_url
-    else
-      @user = @current_user
-    end
-  end
+  before_action :get_current_user
+  before_action :redirect_if_logged_out
+  
+  def index; end
 
   def update
-    @current_user ||= User.find_by(user_id: session[:user_id])
-    if @current_user.nil?
-      redirect_to login_url
-    elsif @current_user.update(user_params)
+    if @current_user.update(user_params)
       redirect_to edit_public_page_path, notice: 'Public page was successfully updated.'
     else
       flash.now[:error] = @current_user.errors.full_messages.to_sentence
@@ -25,6 +16,14 @@ class EditPublicPageController < ApplicationController
   end
 
   private
+
+  def get_current_user
+    @current_user ||= User.find_by(user_id: session[:user_id])
+  end
+  
+  def redirect_if_logged_out
+    redirect_to login_url if @current_user.nil?
+  end
 
   def user_params
     params.require(:user).permit(:fname, :lname, :about, :username)
