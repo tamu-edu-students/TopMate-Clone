a4b030d0a9730571d2f1b2a03b01e7f90aa71015
Ashutosh Punyani <ashutoshspunyani99@tamu.edu>
2023-11-10 21:20:33 -0600

    fixes rspec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,37 +1,8 @@
-# frozen_string_literal: true
-
 # spec/controllers/home_controller_spec.rb
 
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
-  let(:user) { create(:user) }
-
-  describe '#current_user' do
-    it 'does not assign a current user when not logged in' do
-      allow(controller).to receive(:current_user).and_return(nil)
-
-      get :index
-
-      expect(assigns(:current_user)).to be_nil
-    end
-  end
-
-  describe '#require_login' do
-    it 'does not redirect when user is logged in' do
-      allow(controller).to receive(:current_user).and_return(user)
-      get :index
-      expect(response).not_to redirect_to(login_url)
-    end
-  end
-
-  describe '#is_logged_in' do
-    it 'redirects to dashboard page when user is logged in' do
-      allow(controller).to receive(:current_user).and_return(user)
-      get :index
-      expect(response).to redirect_to(dashboard_url)
-    end
-  end
   describe 'GET #index' do
     context 'when the user is logged in' do
       it 'redirects to the dashboard page' do
