3d6c84a408a6af4f93634886c4ac9151b45a62f1
Ashutosh Punyani <ashutoshspunyani99@tamu.edu>
2023-11-13 23:51:57 -0600

    appointment bug fixes

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -24,19 +24,6 @@ class AppointmentsController < ApplicationController
       end
       @slots_data_start_date = next_seven_days
       @slots_data_start_time = []
-      # api_data= get_next_seven_day_time_slots(@user, Date.today)
-      # dummy_slots_data_start_datetime = []
-      # dummy_slots_data_end_datetime = []
-      # @slots=[]
-      #   for i in 0... api_data.length
-      #     if  DateTime.parse(api_data[i]["start_date_time"].to_s)+@service.duration.minutes <= DateTime.parse(api_data[i]["end_date_time"].to_s)
-      #     @slots[i]={'startTime':DateTime.parse(api_data[i]["start_date_time"].to_s),'endTime':DateTime.parse(api_data[i]["end_date_time"].to_s)}
-      #     dummy_slots_data_start_datetime.push(DateTime.parse(api_data[i]["start_date_time"].to_s))
-      #     dummy_slots_data_end_datetime.push(DateTime.parse(api_data[i]["end_date_time"].to_s))
-      #     end
-      #   end
-      #   @slots_data_start_datetime =dummy_slots_data_start_datetime
-      #   @slots_data_end_datetime = dummy_slots_data_end_datetime
     else
       redirect_to public_page_path(@username)
     end
@@ -53,7 +40,6 @@ class AppointmentsController < ApplicationController
     @slots = []
     if api_data.present?
       (0...api_data.length).each do |i|
-        # if  DateTime.parse(api_data[i]["start_date_time"].to_s)+@service.duration.minutes <= DateTime.parse(api_data[i]["end_date_time"].to_s)
         @slots[i] = { 'startTime': api_data[i]['start_time'], 'endTime': api_data[i]['end_time'] }
         dummy_slots_data_start_datetime.push(api_data[i]['start_time'])
         dummy_slots_data_end_datetime.push(api_data[i]['end_time'])
@@ -119,28 +105,16 @@ class AppointmentsController < ApplicationController
     @appointment = Appointments.new(appointments_params)
     flash[:error] ||= ''
     @errors = []
-    @errors.push('first name') if @appointment.fname.nil? || @appointment.fname.empty?
-    @errors.push('last name') if @appointment.lname.nil? || @appointment.lname.empty?
-    @errors.push('email') if @appointment.email.nil? || @appointment.email.empty?
+    @errors.push('First Name') if @appointment.fname.nil? || @appointment.fname.empty?
+    @errors.push('Last Name') if @appointment.lname.nil? || @appointment.lname.empty?
+    @errors.push('Email') if @appointment.email.nil? || @appointment.email.empty?
+    @errors.push('Appointment Date') if @appointment.start_date.nil? || @appointment.start_date.empty?
+    @errors.push('Consultation Start Time') if @appointment.start_time.nil? || @appointment.start_time.empty?
     unless @errors.empty?
       flash[:error] = "Please enter your #{@errors.join(', ')}"
       redirect_to appointments_page_index_path(@username, @service_id)
       return
     end
-
-    # api_data= get_next_seven_day_time_slots(@user, Date.today)
-    # dummy_slots_data_start_datetime = []
-    # dummy_slots_data_end_datetime = []
-    # @slots=[]
-    #   for i in 0... api_data.length
-    #     if  DateTime.parse(api_data[i]["start_date_time"].to_s)+@service.duration.minutes <= DateTime.parse(api_data[i]["end_date_time"].to_s)
-    #     @slots[i]={'startTime':DateTime.parse(api_data[i]["start_date_time"].to_s),'endTime':DateTime.parse(api_data[i]["end_date_time"].to_s)}
-    #     dummy_slots_data_start_datetime.push(DateTime.parse(api_data[i]["start_date_time"].to_s))
-    #     dummy_slots_data_end_datetime.push(DateTime.parse(api_data[i]["end_date_time"].to_s))
-    #     end
-    #   end
-    #   @slots_data_start_datetime =dummy_slots_data_start_datetime
-    #   @slots_data_end_datetime = dummy_slots_data_end_datetime
     @current_start_date = DateTime.parse(params[:appointments][:start_date])
     api_data = user_availability_for_day(@user, @current_start_date)
     dummy_slots_data_start_datetime = []
@@ -157,8 +131,6 @@ class AppointmentsController < ApplicationController
         combined_start_date_time = DateTime.new(date.year, date.month, date.day, start_time.hour, start_time.min, 0)
         combined_end_date_time = DateTime.new(date.year, date.month, date.day, end_time.hour, end_time.min, 0)
 
-        # if  DateTime.parse(api_data[i]["start_date_time"].to_s)+@service.duration.minutes <= DateTime.parse(api_data[i]["end_date_time"].to_s)
-        # @slots[i]={'startTime':api_data[i]["start_time"],'endTime':api_data[i]["end_time"]}
         @slots[i] = { 'startTime': combined_start_date_time, 'endTime': combined_end_date_time }
         dummy_slots_data_start_datetime.push(combined_start_date_time)
         dummy_slots_data_end_datetime.push(combined_end_date_time)
@@ -170,14 +142,12 @@ class AppointmentsController < ApplicationController
     selected_date = Date.parse(@current_start_date.to_s)
     start_time = params[:appointments][:start_time] || api_data[0]['start_time']
     selected_start_time = Time.parse(start_time)
-    # || @slots[0][:start_time]
 
     # Create a DateTime object
     combined_appt_date_time = DateTime.new(selected_date.year, selected_date.month, selected_date.day,
                                            selected_start_time.hour, selected_start_time.min, 0)
     @slots.select do |slot|
       DateTime.parse(slot[:startTime].to_s) == combined_appt_date_time
-      #  DateTime.parse(slot[:startTime].to_s) == DateTime.parse(@appointment.startdatetime.to_s)
     end
     @appointment.startdatetime = combined_appt_date_time
     end_time = @appointment.startdatetime + @service.duration.minutes
